e9c655aa53209ac6a478c6b2a5f38ebb30125f8c
Arvind <arvind.yuvasoft@gmail.com>
2017-03-01 20:17:28 +0530

    fixed routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
    end 
   get 'welcome/index'
   post 'welcome/search'
+  match 'welcome/search', :to => "welcome#search", :as => :search, :via => [:get,:post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
